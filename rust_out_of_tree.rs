42406efa5495bdd76e5d935ff55b312feb99706b
dooraim <dooraim@yahoo.com>
2024-06-30 16:45:49 +0200

    rust_out_of_tree: update example

--- rust_out_of_tree.rs
+++ rust_out_of_tree.rs
@@ -1,37 +1,30 @@
 // SPDX-License-Identifier: GPL-2.0
 
-//! Rust out-of-tree sample
+//! Rust printing macros sample.
 
+use kernel::pr_cont;
 use kernel::prelude::*;
 
 module! {
-    type: RustOutOfTree,
-    name: "rust_out_of_tree",
+    type: RustPrint,
+    name: "rust_print",
     author: "Rust for Linux Contributors",
-    description: "Rust out-of-tree sample",
+    description: "Rust printing macros sample",
     license: "GPL",
 }
 
-struct RustOutOfTree {
-    numbers: Vec<i32>,
-}
+struct RustPrint;
 
-impl kernel::Module for RustOutOfTree {
-    fn init(_module: &'static ThisModule) -> Result<Self> {
-        pr_info!("Rust out-of-tree sample (init)\n");
+impl kernel::Module for RustPrint {
+    fn init(_name: &'static CStr, _module: &'static ThisModule) -> Result<Self> {
+        pr_info!("Hello World from Rust!\n");
 
-        let mut numbers = Vec::new();
-        numbers.push(72, GFP_KERNEL)?;
-        numbers.push(108, GFP_KERNEL)?;
-        numbers.push(200, GFP_KERNEL)?;
-
-        Ok(RustOutOfTree { numbers })
+        Ok(RustPrint)
     }
 }
 
-impl Drop for RustOutOfTree {
+impl Drop for RustPrint {
     fn drop(&mut self) {
-        pr_info!("My numbers are {:?}\n", self.numbers);
-        pr_info!("Rust out-of-tree sample (exit)\n");
+        pr_info!("Rust printing macros sample (exit)\n");
     }
 }
